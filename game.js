e46189eac43e9f1e0076b903ef7dec2f6e5673d7
Taíse da Rosa <taise.rosa@aluno.fmpsc.edu.br>
2025-06-26 14:10:19 -0300

    Update game.js

--- game.js
+++ game.js
@@ -160,7 +160,7 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('images/${character}.jpeg')`;
+    front.style.backgroundImage = `url(${character}.jpeg')`;
 
     card.appendChild(front);
     card.appendChild(back);
